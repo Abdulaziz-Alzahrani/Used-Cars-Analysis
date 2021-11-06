6faeff289722c438784a6a81bcaa33246a6daf38
Abdulaziz-Alzahrani <87986521+Abdulaziz-Alzahrani@users.noreply.github.com>
2021-11-06 15:12:58 +0300

    updating repo content

--- Readme.md
+++ Readme.md
@@ -1,8 +1,8 @@
-# Introduction: 
-this is a project done to perform analysis on used cars dataset from [Kaggle](https://www.kaggle.com/ammaraahmad/used-cars-dataset)
+## Project Motivation 
 
-please visit the blog post on [Medium](https://medium.com/@aalzahrani8991/characteristics-of-used-cars-and-its-effects-on-price-492b78f64945) 
-to learn more about this project.
+Cars are made up of thousands of diffrent parts and there are many manufacturers resulting in alot of 
+elements that might or might not effect the price of a used car.
+In this Project we want to see what characteristics of used cars and its effects on price. 
 
 ## Installation
 
@@ -24,3 +24,38 @@ pip install matplotlib
 ```bash
 pip install seaborn
 ```
+
+## Content
+
+#### - used_cars_analysis.py :
+a python file containing all the code needed to answer the questions related to the project.
+
+#### - usedcar_dataset_analysis.ipynb :
+a Jupyter notebook containing the objectives, questions, analysis and visualization related to the project.
+
+#### - usedcars_dataset.csv :
+comma seperated file containing the dataset used in this project.
+
+#### - Readme.md :
+description of the project.
+
+## Summary:
+In this project I did the analysis about the characteristics of a car and its effects on its resell price and the following is a summary of the results that I found.
+
+Car Make can have an effect on price.
+Body style of a car can effect price.
+The Powertrain can impact the price of a car.
+
+## Acknowledgements 
+
+I would like to express my gratitude to Misk Academy and Udacity for this Amazing program
+that expanded my knowledge and helped me in making this project.
+
+## More info and Refrences
+
+- dataset from [Kaggle](https://www.kaggle.com/ammaraahmad/used-cars-dataset)
+
+please visit the blog post on [Medium](https://medium.com/@aalzahrani8991/characteristics-of-used-cars-and-its-effects-on-price-492b78f64945) 
+to learn more about this project.
+
+
--- used_cars_analysis.py
+++ used_cars_analysis.py
@@ -4,50 +4,120 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-#get every brand and its avg price
+
 def make_and_price(data):
+    """
+   get every brand and its avg price and plot the relation between them as a bar graph.
+  
+    Parameters:
+    data (pandas.DataFrame): a DataFrame containing the data after it has been cleaned.
+  
+    Returns:
+    None
+  
+    """
     makes_avgs = data.groupby('make')['price'].mean()
     makes_avgs.plot.bar(x='make', y='price')
     plt.show()
     plt.close()
 
-# get the body style relation to price
+
 def bodystyle_and_price(data):
+    """
+   get every body style and its avg price and plot the relation between them as a bar graph.
+  
+    Parameters:
+    data (pandas.DataFrame): a DataFrame containing the data after it has been cleaned.
+  
+    Returns:
+    None
+  
+    """
     body_style_and_price = data.groupby("body-style")['price'].mean()
     body_style_and_price.plot.bar(x='body-style', y='price')
     plt.show()
     plt.close()
 
-# get the length relation to price
+
 def length_and_price(data):
+    """
+   get the length relation to price and plot it as a line graph.
+  
+    Parameters:
+    data (pandas.DataFrame): a DataFrame containing the data after it has been cleaned.
+  
+    Returns:
+    None
+  
+    """
     body_style_and_price = data.groupby('length')['price'].mean()
     body_style_and_price.plot.line(x='length', y='price')
     plt.show()
     plt.close()
 
-# get the width relation to price
+
 def width_and_price(data):
+    """
+  get the width relation to price and plot it as a line graph.
+  
+    Parameters:
+    data (pandas.DataFrame): a DataFrame containing the data after it has been cleaned.
+  
+    Returns:
+    None
+  
+    """
     body_style_and_price = data.groupby('width')['price'].mean()
     body_style_and_price.plot.line(x='width', y='price')
     plt.show()
     plt.close()
 
-# get the horsepower relation to price
+
 def horsepower_and_price(data):
+    """
+   get the horsepower relation to price and plot it as a line graph.
+  
+    Parameters:
+    data (pandas.DataFrame): a DataFrame containing the data after it has been cleaned.
+  
+    Returns:
+    None
+  
+    """
     body_style_and_price = data.groupby('horsepower')['price'].mean()
     body_style_and_price.plot.line(x='horsepower', y='price')
     plt.show()
     plt.close()
 
-# get the drivewheel relation to price
+
 def drivewheel_and_price(data):
+    """
+   get every drive wheel type and its avg price and plot the relation between them as a bar graph.
+  
+    Parameters:
+    data (pandas.DataFrame): a DataFrame containing the data after it has been cleaned.
+  
+    Returns:
+    None
+  
+    """
     makes_avgs = data.groupby('drive-wheels')['price'].mean()
     makes_avgs.plot.bar(x='drive-wheels', y='price')
     plt.show()
     plt.close()
 
-# get the gas vs diesel relation to price
+
 def gas_diesel_price(data):
+    """
+   get diesel and gasoline fuel types and their avg price and plot the relation between them as a bar graph.
+  
+    Parameters:
+    data (pandas.DataFrame): a DataFrame containing the data after it has been cleaned.
+  
+    Returns:
+    None
+  
+    """
     makes_avgs = data.groupby('diesel')['price'].mean()
     makes_avgs = makes_avgs.rename({0:'Gasoline', 1:'Diesel'})
     makes_avgs.plot.bar(y='price')
